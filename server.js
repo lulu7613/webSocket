b68d71f24d39a298955bb2f66d7cd6023ab7cc01
Lulu Tsai <lulu_tsai@myplay20.com>
2020-12-30 16:38:41 +0800

    webSocket

---
+++ server.js
@@ -0,0 +1,53 @@
+// import express 和 ws 套件
+const express = require('express')
+const SocketServer = require('ws').Server
+
+// 指定開啟的 port
+const PORT = 3000
+
+// 創建 express 的物件，並綁定及監聽 3000 port ，且設定開啟後在 console 中提示
+const server = express()
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+// 將 express 交給 SocketServer 開啟 WebSocket 的服務
+const wss = new SocketServer({ server })
+
+// 當 WebSocket 從外部連結時執行
+wss.on('connection', ws => {
+
+    // 連結時執行此 console 提示
+    console.log('Client connected')
+
+    // 對 message 設定監聽，接收從 Client 發送的訊息
+    ws.on('message', data => {
+      // data 為 Client 發送的訊息，現在將訊息原封不動發送出去
+      // ws.send(data)
+
+      // 取得所有連接中的 client
+      const clients = wss.clients
+
+      // 做迴圈，發送訊息至每個 client
+      clients.forEach(client => {
+        client.send(data)
+      })
+    })
+
+    // 當 WebSocket 的連線關閉時執行
+    ws.on('close', () => {
+      console.log('Close connected')
+      // clearInterval(sendNowTime)
+    })
+
+
+    // ------ 發送訊息 ------
+
+    // 固定送最新時間給 Client
+    // const sendNowTime = setInterval(()=>{
+    //   ws.send(String(new Date()))
+    // },1000)
+    // ws.send('伺服器傳送訊息囉!')
+    
+    setTimeout(() => {
+      ws.send('已經過15秒啦! 伺服器傳送訊息囉!')
+    }, 15000)
+})
